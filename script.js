812a38706bf34be0022ce97ac70eff5c5de67e17
Anton Nakonechny <kashot.kukuskin@gmail.com>
2020-03-22 18:15:48 +0300

    feat: made the switching links when scrolling

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
 const nav = document.querySelector(".main-navigation__list");
-nav.addEventListener('click', changeActiveItemToHeader);
-document.addEventListener('scroll', scrollPage);
+const navigationItems = nav.querySelectorAll(".main-navigation__item");
+nav.addEventListener("click", scrollPage);
+document.addEventListener("scroll", pageScrollingIdentification);
 
-function changeActiveItemToHeader(e) {
-  const arrItem = nav.querySelectorAll(".main-navigation__item");
-  arrItem.forEach(el => el.classList.remove("main-navigation__item--active"));
-  const parentEl = e.target.parentNode;
-  if(Array.prototype.includes.call(arrItem, parentEl))
+function disableNavigationItems() {
+  navigationItems.forEach(el =>
+    el.classList.remove("main-navigation__item--active")
+  );
+}
+
+function changeActiveItemToHeader(el) {
+  const parentEl = el.parentNode;
+  if (Array.prototype.includes.call(navigationItems, parentEl)) {
+    disableNavigationItems();
     parentEl.classList.add("main-navigation__item--active");
+  }
 }
 
 function scrollPage(e) {
-  let currentScrollValue = window.pageYOffset;
+  e.preventDefault();
+  const current = e.target;
+  if (current.href) {
+    const el = document.querySelector(
+      current.href.slice(current.href.indexOf("#"))
+    );
+    window.scrollTo({
+      top: el.offsetTop - 95
+    });
+    changeActiveItemToHeader(current);
+  }
+}
+
+function getSizeBlocks(nameClass) {
   const sections = document.querySelectorAll(".block");
-  
-  console.log(sections);
+  if (sections.length === 0) throw new Error(`element ${nameClass} not found`);
+  const sizeBlocks = [];
+  for (const iterator of sections) {
+    sizeBlocks.push(iterator.scrollHeight);
+  }
+  return sizeBlocks;
+}
+
+function pageScrollingIdentification() {
+  let currentScrollValue = window.pageYOffset;
+  disableNavigationItems();
+
+  let sizeBlocks = [];
+  try {
+    sizeBlocks = getSizeBlocks(".block");
+  } catch (e) {
+    console.error(e.message);
+  }
+
+  let currentSizeScrollingBlocks = 0;
+  for (let index = 0; index < sizeBlocks.length; index++) {
+    currentSizeScrollingBlocks += sizeBlocks[index];
+    if (
+      currentScrollValue + window.screen.availHeight / 2 - 95 <
+      currentSizeScrollingBlocks
+    ) {
+      navigationItems[index].classList.add("main-navigation__item--active");
+      break;
+    }
+  }
 }
