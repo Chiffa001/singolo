03db9a7d6a825f02b1e19b612e733954dc0b378e
Anton Nakonechny <kashot.kukuskin@gmail.com>
2020-03-22 20:23:53 +0300

    feat: do toggle the screens of phones

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector(".main-navigation__list");
 const navigationItems = nav.querySelectorAll(".main-navigation__item");
+const header = document.querySelector(".main-header");
+
 nav.addEventListener("click", scrollPage);
 document.addEventListener("scroll", pageScrollingIdentification);
 
@@ -10,10 +12,9 @@ function disableNavigationItems() {
 }
 
 function changeActiveItemToHeader(el) {
-  const parentEl = el.parentNode;
-  if (Array.prototype.includes.call(navigationItems, parentEl)) {
+  if (Array.prototype.includes.call(navigationItems, el)) {
     disableNavigationItems();
-    parentEl.classList.add("main-navigation__item--active");
+    el.classList.add("main-navigation__item--active");
   }
 }
 
@@ -25,9 +26,9 @@ function scrollPage(e) {
       current.href.slice(current.href.indexOf("#"))
     );
     window.scrollTo({
-      top: el.offsetTop - 95
+      top: el.offsetTop - header.scrollHeight
     });
-    changeActiveItemToHeader(current);
+    changeActiveItemToHeader(current.parentNode);
   }
 }
 
@@ -56,11 +57,21 @@ function pageScrollingIdentification() {
   for (let index = 0; index < sizeBlocks.length; index++) {
     currentSizeScrollingBlocks += sizeBlocks[index];
     if (
-      currentScrollValue + window.screen.availHeight / 2 - 95 <
+      currentScrollValue + window.screen.availHeight / 2 - header.scrollHeight <
       currentSizeScrollingBlocks
     ) {
       navigationItems[index].classList.add("main-navigation__item--active");
       break;
     }
   }
+}
+
+// phone click
+
+const screensPhones = document.querySelectorAll(".slide-1 .slider__background");
+screensPhones.forEach(screen => screen.addEventListener("click", toggleScreen));
+
+function toggleScreen(e) {
+  const current = e.target;
+  current.classList.toggle("slider__background--hidden");
 }
